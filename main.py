773dc9b45bd2ae2353e6f3f22b17835aae8842f5
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-05-16 13:03:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,23 +12,27 @@ with header:
 
 with kpi_selection:
     st.header("KPI Selection")
-    firstKPI, secondKPI, thirdKPI, clear = st.columns([0.5, 0.5, 1, 1])
+    firstKPI, secondKPI, thirdKPI, clear = st.columns([0.5, 0.4, 1, 1])
     with firstKPI:
         loneliness_kpi_button = st.button("Loneliness")
-    
+
     with secondKPI:
         health_kpi_button = st.button("Health")
-    
+
     with thirdKPI:
         economic_strength_kpi_button = st.button("Economic Strength")
-    
+
     with clear:
         clear_button = st.button("Clear")
 
 with kpi_weights:
     st.header("KPI weights")
     if loneliness_kpi_button:
-        pass
+        total_points = st.slider("arnona_cat", 0, 1, 0.15)
+        # loneliness_weights = [["arnona_cat", 0.15], ["members_Water", 0.15], ["martial", 0.15], ["widow_grown", 0.04],
+        #                       ["widow_elderlies", 0.1], ["lonely_elderlies", 0.3], ["p85_plus", 0.06],
+        #                       ["accumulated_cases", 0.05]]
+
     elif health_kpi_button:
         pass
     elif economic_strength_kpi_button:
