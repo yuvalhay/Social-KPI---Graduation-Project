4df865be81b180e19f3b884c54ca2bc707dde0bd
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-05-19 19:06:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 import setup
+from streamlit_option_menu import option_menu
+
 # import streamlit.components.v1 as html
 # from PIL import Image
 # import cv2
