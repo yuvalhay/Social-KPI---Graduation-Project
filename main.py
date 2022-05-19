006bdf19301023eeb0bf7ce71724ec28d0eca4f8
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-05-19 19:35:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,5 @@
 import streamlit as st
-import setuptools
-
-setuptools.setup(
-    name="streamlit-option-menu",
-    version="0.3.2",
-    author="Victor Yan",
-    author_email="victoryhb@163.com",
-    description="streamlit-option-menu is a simple Streamlit component that allows users to select a single item from a list of options in a menu.",
-    long_description="""streamlit-option-menu is a simple Streamlit component that allows users to select a single item from a list of options in a menu.
-It is similar in function to st.selectbox(), except that:
-- It uses a simple static list to display the options instead of a dropdown
-- It has configurable icons for each option item and the menu title
-It is built on [streamlit-component-template-vue](https://github.com/andfanilo/streamlit-component-template-vue), styled with [Bootstrap](https://getbootstrap.com/) and with icons from [bootstrap-icons](https://icons.getbootstrap.com/)
-## Installation
-```
-pip install streamlit-option-menu
-```
-## Parameters
-The `option_menu` function accepts the following parameters:
-- menu_title (required): the title of the menu; pass None to hide the title
-- options (required): list of (string) options to display in the menu; set an option to "---" if you want to insert a section separator
-- default_index (optional, default=0): the index of the selected option by default
-- menu_icon (optional, default="menu-up"): name of the [bootstrap-icon](https://icons.getbootstrap.com/) to be used for the menu title
-- icons (optional, default=["caret-right"]): list of [bootstrap-icon](https://icons.getbootstrap.com/) names to be used for each option; its length should be equal to the length of options
-- orientation (optional, default="vertical"): "vertical" or "horizontal"; whether to display the menu vertically or horizontally
-The function returns the (string) option currently selected
-## Example
-```
-import streamlit as st
-from streamlit_option_menu import option_menu
-with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
-# horizontal Menu
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
-    selected2
-```
-""",
-    long_description_content_type="text/plain",
-    url="https://github.com/yuvalhay/Social-KPI---Graduation-Project/streamlit-option-menu",
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    classifiers=[],
-    python_requires=">=3.6",
-    install_requires=[
-        # By definition, a Custom Component depends on Streamlit.
-        # If your component has other Python dependencies, list
-        # them here.
-        "streamlit >= 0.63",
-    ],
-)
-from streamlit_option_menu import option_menu
-
+# from streamlit_option_menu import option_menu
 # import streamlit.components.v1 as html
 # from PIL import Image
 # import cv2
@@ -65,23 +10,24 @@ from streamlit_option_menu import option_menu
 header = st.container()
 kpi_selection = st.container()
 kpi_weights = st.container()
+Loneliness_default_values = [0.15, 0.15, 0.15, 0.04, 0.1, 0.3, 0.06, 0.05]
 
-with st.sidebar:
-    # st.sidebar
-    # options_names = ["Prediction", "KPI"]
-    # choose_page = st.radio("Choose", options_names)
-
-    choose = option_menu("App Gallery", ["About", "Prediction", "Social KPI", "Contact"],
-                         icons=['person lines fill', 'pc display horizontal', 'people', 'pencil square'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-                             "container": {"padding": "5!important", "background-color": "orange"},
-                             "icon": {"color": "black", "font-size": "25px"},
-                             "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px",
-                                          "--hover-color": "#eee"},
-                             "nav-link-selected": {"background-color": "#02ab21"},
-                         }
-                         )
+# with st.sidebar:
+#     # st.sidebar
+#     # options_names = ["Prediction", "KPI"]
+#     # choose_page = st.radio("Choose", options_names)
+#
+#     choose = option_menu("App Gallery", ["About", "Prediction", "Social KPI", "Contact"],
+#                          icons=['person lines fill', 'pc display horizontal', 'people', 'pencil square'],
+#                          menu_icon="app-indicator", default_index=0,
+#                          styles={
+#                              "container": {"padding": "5!important", "background-color": "orange"},
+#                              "icon": {"color": "black", "font-size": "25px"},
+#                              "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px",
+#                                           "--hover-color": "#eee"},
+#                              "nav-link-selected": {"background-color": "#02ab21"},
+#                          }
+#                          )
 
 with header:
     st.title("The visualization of our KPI's")
@@ -125,6 +71,7 @@ with kpi_weights:
         p85_plus = st.slider("p85_plus", 0.0, 1.0, Loneliness_default_values[6])
         accumulated_cases = st.slider("accumulated_cases", 0.0, 1.0, Loneliness_default_values[7])
 
+        st.text(f"{arnona_cat}")
         if arnona_cat != current_values[0]:
             diff_val = arnona_cat - current_values[0]
             avg_diff = diff_val/7  # בכמה לשנות כל משקל
