05959d3e7a14595c0b4251ffbdb8f86545ad94a1
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-05-23 19:54:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ with kpi_weights:
     elif KPI_page == "Health":
         st.snow()
         my_bar = st.progress(0)
-        for percent_complete in range(50):
+        for percent_complete in range(100):
             time.sleep(0.1)
             my_bar.progress(percent_complete + 1)
     elif KPI_page == "Economic Strength":
