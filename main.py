a9167478c1be66dd3e9131ec16a1f733ac9a8f5a
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-05-27 18:38:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import time
 from streamlit_option_menu import option_menu
+import pandas as pd
+import numpy as np
 # import streamlit.components.v1 as html
 # from PIL import Image
 # import cv2
