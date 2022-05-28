6d0fbcbb9f12f41d1d68d095b4d89409b6c4992b
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-05-28 19:50:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ with st.sidebar:
                              "icon": {"color": "black", "font-size": "25px"},
                              "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px",
                                           "--hover-color": "#eee"},
-                             "nav-link-selected": {"background-color": "#02ab21"},
+                             "nav-link-selected": {"background-color": "#ff0000"},
                          }
                          )
 
