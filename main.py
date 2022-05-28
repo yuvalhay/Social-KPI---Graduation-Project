5201d330c368f1990ccd19d378c2311cbd234829
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-05-28 21:10:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -146,16 +146,16 @@ elif choose == "Social KPI":
             #     economic_strength_kpi_button = st.button("Economic Strength")
             # with clear:
             #     clear_button = st.button("Clear")
-    
+
         with kpi_weights:
             header("KPI weights")
             current_values = [0.15, 0.15, 0.15, 0.04, 0.1, 0.3, 0.06, 0.05]
             Loneliness_kpi_dict = {"arnona_cat": 0, "members_Water": 0, "martial": 0, "widow_grown": 0, "widow_elderlies": 0,
                                    "lonely_elderlies": 0, "p85_plus": 0, "accumulated_cases": 0}
-    
+
             basic_ratio = [3, 3, 3, 1, 2, 6, 1, 1]
             current_ratio = [3, 3, 3, 1, 2, 6, 1, 1]
-    
+
             if KPI_page == "Loneliness":
                 # st.balloons()
                 Loneliness_kpi_dict_keys = list(Loneliness_kpi_dict.keys())
@@ -164,12 +164,12 @@ elif choose == "Social KPI":
                     Loneliness_kpi_dict[key] = st.select_slider('Explanation', options=[1, 2, 3, 4, 5, 6, 7],
                                                                 value=current_ratio[index], key=Loneliness_kpi_dict_keys[index])
                     index += 1
-    
+
                 sum_of_weights = round(sum(list(Loneliness_kpi_dict.values())), 5)
                 st.write(sum_of_weights)
                 Loneliness_weights_dict = {key: round(weight/sum_of_weights, 5) for key, weight in Loneliness_kpi_dict.items()}
                 st.write(Loneliness_weights_dict)
-    
+
             elif KPI_page == "Health":
                 st.title("Working on it")
                 # st.snow()
@@ -178,10 +178,24 @@ elif choose == "Social KPI":
                 #     time.sleep(0.1)
                 #     my_bar.progress(percent_complete + 1)
             elif KPI_page == "Economic Strength":
-                with st.spinner('Exporting File..'):
-                    time.sleep(3)
-                st.success('Done!')
-    
+                # st.balloons()
+                Loneliness_kpi_dict_keys = list(Loneliness_kpi_dict.keys())
+                index = 0
+                for key in Loneliness_kpi_dict.keys():
+                    Loneliness_kpi_dict[key] = st.select_slider('Explanation', options=[1, 2, 3, 4, 5, 6, 7],
+                                                                value=current_ratio[index],
+                                                                key=Loneliness_kpi_dict_keys[index])
+                    index += 1
+
+                sum_of_weights = round(sum(list(Loneliness_kpi_dict.values())), 5)
+                st.write(sum_of_weights)
+                Loneliness_weights_dict = {key: round(weight / sum_of_weights, 5) for key, weight in
+                                           Loneliness_kpi_dict.items()}
+                st.write(Loneliness_weights_dict)
+                # with st.spinner('Exporting File..'):
+                #     time.sleep(3)
+                # st.success('Done!')
+
             # health_weights = [["arnona_cat", 0.2], ["age", 0.08], ["hashlama_kizvat_nechut_elderlies", (-2) * 0.08],
             #                   ["Mekabley_kizbaot_nechut", (-2) * 0.1], ["zachaim_kizbat_nechut_children", (-2) * 0.09],
             #                   ["mekabley_kizbaot_from_injured_Work", (-2) * 0.11], ["mekabley_kizba_siud", (-2) * 0.15],
@@ -197,7 +211,7 @@ elif choose == "Social KPI":
             #                              ["Mekabley_kizbaot_nechut", 0.022], ["zachaim_kizbat_nechut_children", 0.022],
             #                              ["mekabley_kizbaot_from_injured_Work", 0.022], ["mekabley_kizba_siud", 0.022],
             #                              ["socio_economic", 0.1], ["area_per_person", 0.03]]
-    
+
             #     st.text("In this section you will see all the weights that create the KPI you selected")
 
 elif choose == "Contact":
