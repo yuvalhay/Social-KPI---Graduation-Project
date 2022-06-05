3c6023edc14a86de5735a1ed1b6febd552f52ce0
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-06-05 15:34:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from PIL import Image
 # from st_aggrid import AgGrid
 # import plotly.express as px
 # import io
+st.set_page_config(layout="wide")
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
