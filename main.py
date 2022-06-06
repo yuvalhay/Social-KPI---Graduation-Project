899e6081a4e47415dcd827bfdfee1b4e9d900340
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-06-06 18:09:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ elif choose == "Social KPI":
                     radius=40,
                     auto_highlight=True,
 #                     get_radius=10000,          # Radius is given in meters
-                    get_fill_color=["255", "Loneliness_score * 6 + 30", "Loneliness_score * 6", "Loneliness_score / 5"],  # Set an RGBA value for fill
+                    get_fill_color=["255 - Loneliness_score * 10", "Loneliness_score * 6 + 30", "Loneliness_score * 6", "Loneliness_score / 5"],  # Set an RGBA value for fill
 #                     elevation_range=[0, 1000],
                     pickable=True,
                     extruded=True,
