d665872bca3ddb917d28a8fe8ceee3fd24a851ad
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-06-06 13:23:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -194,6 +194,34 @@ elif choose == "Social KPI":
                 Loneliness_weights_dict = {key: round(weight/sum_of_weights, 5) for key, weight in Loneliness_kpi_dict.items()}
                 st.write(Loneliness_weights_dict)
                 st.map(map_df, zoom=13)
+                st.pydeck_chart(pdk.Deck(
+                     map_style='mapbox://styles/mapbox/light-v9',
+                     initial_view_state=pdk.ViewState(
+                         latitude=37.76,
+                         longitude=-122.4,
+                         zoom=11,
+                         pitch=50,
+                     ),
+                     layers=[
+                         pdk.Layer(
+                            'HexagonLayer',
+                            data=df,
+                            get_position='[lon, lat]',
+                            radius=200,
+                            elevation_scale=4,
+                            elevation_range=[0, 1000],
+                            pickable=True,
+                            extruded=True,
+                         ),
+                         pdk.Layer(
+                             'ScatterplotLayer',
+                             data=df,
+                             get_position='[lon, lat]',
+                             get_color='[200, 30, 0, 160]',
+                             get_radius=200,
+                         ),
+                     ],
+                 ))
                 # get df from model.py after multiply it by the new weights and group by it by statistical area (average) 
                 # st.table(df)
 
