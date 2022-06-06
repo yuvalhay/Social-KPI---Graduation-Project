10017db099e67c0d4bd46f209d76d1f124dfcd5d
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-06-06 15:59:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -214,7 +214,7 @@ elif choose == "Social KPI":
                     map_df,
                     get_position=['lon', 'lat'],
                     auto_highlight=True,
-                    get_radius=1000,          # Radius is given in meters
+                    get_radius=10,          # Radius is given in meters
                     get_fill_color=[180, 0, 200, 140],  # Set an RGBA value for fill
                     pickable=True,
                     coverage=0.1)
