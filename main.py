4dc763e8ca3c8bd682f69783efd55caa1a732736
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-06-06 14:19:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -195,13 +195,21 @@ elif choose == "Social KPI":
                 st.write(sum_of_weights)
                 Loneliness_weights_dict = {key: round(weight/sum_of_weights, 5) for key, weight in Loneliness_kpi_dict.items()}
                 st.write(Loneliness_weights_dict)
-                st.map(map_df, zoom=13)
-                layer = pdk.Layer("GridLayer", df, pickable=True, extruded=True, cell_size=200, elevation_scale=4, get_position=["lat", "lon"])
-                view_state = pdk.ViewState(latitude=37.7749295, longitude=-122.4194155, zoom=11, bearing=0, pitch=45)
+#                 st.map(map_df, zoom=13)
+                layer = pdk.Layer(
+                            'HexagonLayer',  # `type` positional argument is here
+                            map_df,
+                            get_position=['lng', 'lat'],
+                            auto_highlight=True,
+                            elevation_scale=50,
+                            pickable=True,
+                            elevation_range=[0, 3000],
+                            extruded=True,
+                            coverage=1)
 
                 # Render
-                r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={"text": "{position}\nCount: {count}"},)
-                r.to_html("grid_layer.html")
+                r = pdk.Deck(layers=[layer])
+                r.to_html()
 #                 weights_update()
 #                 st.pydeck_chart(pdk.Deck(
 #                      map_style='mapbox://styles/mapbox/light-v9',
