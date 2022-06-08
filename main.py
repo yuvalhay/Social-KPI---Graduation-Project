75eb614cd3829bc5de2bfe63a46942b3a41433ff
Niv Shtaif <73525959+nivshtaif@users.noreply.github.com>
2022-06-08 20:43:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from functioned import *
 # import io
 st.set_page_config(layout="wide")
 # set_png_as_page_bg('background_img/3_background_img_1920_1080.png')
-set_png_as_sidebar_bg('background_img/3_background_img_1920_1080.png')
 #     st.markdown("""<style>.sidebar .sidebar-content {background-image: linear-gradient(#2e7bcf,#2e7bcf);color: red;}</style>""",unsafe_allow_html=True,)
 
 
