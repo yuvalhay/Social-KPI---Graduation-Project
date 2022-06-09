f164545f398cb0e3ea9a025ec0f2cc88f13cabdc
yuvalhay <76970469+yuvalhay@users.noreply.github.com>
2022-06-09 14:39:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -124,31 +124,31 @@ if choose == "File Upload":
             #     global map_df
                 map_df = df_scored[["lat", "lon", "Loneliness_score", "Health_score", "Economic_Strength_score"]]
 
-                R_color, G_color = [], []
-                num_of_rows = df_scored.shape[0]
-                num_of_rows_range = [i for i in range(num_of_rows)]
-                for v in list(map_df["Loneliness_score"]):
-    #                     st.write(v)
-                    if v == 1:
-                        R_color.append(44)
-                        G_color.append(186)
-                    elif v == 2:
-                        R_color.append(163)
-                        G_color.append(255)
-                    elif v == 3:
-                        R_color.append(255)
-                        G_color.append(244)
-                    elif v == 4:
-                        R_color.append(255)
-                        G_color.append(167)
-                    elif v == 5:
-                        R_color.append(255)
-                        G_color.append(0)
+#                 R_color, G_color = [], []
+#                 num_of_rows = df_scored.shape[0]
+#                 num_of_rows_range = [i for i in range(num_of_rows)]
+#                 for v in list(map_df["Loneliness_score"]):
+#     #                     st.write(v)
+#                     if v == 1:
+#                         R_color.append(44)
+#                         G_color.append(186)
+#                     elif v == 2:
+#                         R_color.append(163)
+#                         G_color.append(255)
+#                     elif v == 3:
+#                         R_color.append(255)
+#                         G_color.append(244)
+#                     elif v == 4:
+#                         R_color.append(255)
+#                         G_color.append(167)
+#                     elif v == 5:
+#                         R_color.append(255)
+#                         G_color.append(0)
 
-    #             map_df["R_color"] = R_color
-    #             map_df["G_color"] = G_color
-                st.session_state["R_color"] = R_color
-                st.session_state["G_color"] = G_color
+#     #             map_df["R_color"] = R_color
+#     #             map_df["G_color"] = G_color
+#                 st.session_state["R_color"] = R_color
+#                 st.session_state["G_color"] = G_color
                 st.session_state['map_df'] = map_df
             
             st.success("File was uploaded!")
@@ -264,7 +264,7 @@ elif choose == "Social KPI" and st.session_state['flag'] is True:
                 curr_df = MetricsCalc(st.session_state['df_scored'], loneliness_dict, st.session_state['health_dict'], st.session_state['economic_strength_dict'], True)
 #                 update_session_state("df_scores", curr_df)
                 R_color, G_color = [], []
-                num_of_rows = df_scored.shape[0]
+                num_of_rows = curr_df.shape[0]
                 num_of_rows_range = [i for i in range(num_of_rows)]
                 for v in list(curr_df["Loneliness_score"]):
     #                     st.write(v)
