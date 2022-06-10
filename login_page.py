df139c38cbc48934fe5b4be71638bc70cf23efd1
Niv Shtaif <73525959+nivshtaif@users.noreply.github.com>
2022-06-10 13:01:22 +0300

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -5,9 +5,9 @@ def loginn():
     names = st.secrets["DB_USERNAME"]
     usernames = st.secrets["DB_USERNAME"]
     passwords = st.secrets["DB_TOKEN"]
-#     hashed_passwords = stauth.Hasher(passwords).generate()
+    hashed_passwords = stauth.Hasher(passwords).generate()
 
-    authenticator = stauth.Authenticate(names,usernames,passwords,
+    authenticator = stauth.Authenticate(names,usernames,hashed_passwords,
                                       'some_cookie_name','some_signature_key',cookie_expiry_days=30)
 
     name, authentication_status, username = authenticator.login('Login','main')
