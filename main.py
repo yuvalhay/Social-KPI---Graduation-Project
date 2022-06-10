e9f7dad1f7d531ce04248c191b77b890712889c5
Niv Shtaif <73525959+nivshtaif@users.noreply.github.com>
2022-06-10 14:00:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -753,6 +753,52 @@ def main():
 #     elif authentication_status == None:
 #         st.warning('Please enter your username and password')
 
+
+# streamlit_app.py
+
+import streamlit as st
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+# if check_password():
+#     st.write("Here goes your normal Streamlit app...")
+#     st.button("Click me")
+
+
+
 if __name__ == "__main__":
 #     St_login()
 #     main()
@@ -768,6 +814,12 @@ if __name__ == "__main__":
         st.error('Username/Password is incorrect')
     elif authentication_status == None:
         st.warning('Please enter your username and password')
+        
+        
+if check_password():
+    st.write("Here goes your normal Streamlit app...")
+    st.button("Click me")
+    main()
         
 #         st.subheader("Register")
 #         with st.expander("Registering"):
