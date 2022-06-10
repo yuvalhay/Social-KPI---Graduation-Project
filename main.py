04377faf9a9fa63d227935b8af7a6c9d44fab300
Niv Shtaif <73525959+nivshtaif@users.noreply.github.com>
2022-06-10 14:13:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -787,7 +787,7 @@ def check_password():
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
-        st.error("😕 User not known or password incorrect")
+        st.error("Username/Password is incorrect 😕 Try Again")
         return False
     else:
         # Password correct.
