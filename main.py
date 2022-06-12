cf7384f75cf6f80647243e5d3d73254ee07380c6
Niv Shtaif <73525959+nivshtaif@users.noreply.github.com>
2022-06-12 18:15:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -855,6 +855,20 @@ def main():
 #                 col3.subheader("")
 #                 st.metric(label="Households which are under risk", value=f'{round(perc_risk,3)}%')
                 st.title(f'{round(perc_risk,3)}% of the households are under risk')
+                import matplotlib.pyplot as plt
+
+                # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+                labels = 'Risk', ''
+                sizes = [perc_risk, 1-perc_risk]
+                explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+                fig1, ax1 = plt.subplots()
+                ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+                        shadow=True, startangle=90)
+                ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+                st.pyplot(fig1)
+                
                 
                 st.dataframe(df_risk)
 
