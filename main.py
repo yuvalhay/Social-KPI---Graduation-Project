077bef0b03973250769ffdb00e8dd83f3f6c45e6
Niv Shtaif <73525959+nivshtaif@users.noreply.github.com>
2022-06-12 09:50:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,8 @@ def update_session_state(key, value):
 
 def main():
     with st.sidebar:
+        image = Image.open(r'Team_members_pictures/Tal.jpeg')
+        st.image(image, width= use_column_width=True)
         choose = option_menu("SoCity", ["File Upload", "Social KPI", "Prediction", "About"],
                              icons=['upload', 'sliders', 'kanban', 'person lines fill'],
 
