811b085522bdbbc31129494ca29f73c699a49367
Niv Shtaif <73525959+nivshtaif@users.noreply.github.com>
2022-06-12 09:43:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
         choose = option_menu("SoCity", ["File Upload", "Social KPI", "Prediction", "About"],
                              icons=['upload', 'sliders', 'kanban', 'person lines fill'],
 
-                             menu_icon="background_img/0678aa5950dac123c2d24619e0162e28.svg", default_index=0,
+                             menu_icon="background_img/favicon.ico", default_index=0,
     #                          bi bi-building
     #                          app-indicator
                              styles={
