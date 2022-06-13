36965e2164b809625f4228b53b935587edd1f82b
Niv Shtaif <73525959+nivshtaif@users.noreply.github.com>
2022-06-13 14:37:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ set_png_as_page_bg('background_img/Simple Cute Desktop Wallpapers - WallpaperSaf
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+	
 
 login_icon = st.container()
 
